74f26a8cda7caed62e3f01aae009083e1052bf64
Emma-90844 <enabojohn@gmail.com>
2024-08-26 01:35:32 +0300

    Innitial commit

--- app/index.jsx
+++ app/index.jsx
@@ -1,6 +1,7 @@
 import { Redirect } from "expo-router";
-import { Text, View } from "react-native";
+import { Text, View , StyleSheet} from "react-native";
 
 export default function Index() {
   return <Redirect href={'/home'}/>
+
 }
